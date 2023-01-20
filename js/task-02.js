b26b3372b1840a9d69017da96f5d80515bf8f818
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-01-20 20:36:00 +0200

    ready second

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+  console.log(ingredient);
+  const heading = document.createElement("li");
+  heading.textContent = `${ingredient}`;
+  list.append(heading);
+}
