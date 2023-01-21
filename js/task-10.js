fdf0107ab6e90e4076938b3429845433470c221a
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-01-22 01:17:53 +0200

    finish

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  amountEl: document.querySelector("#controls input"),
+  createEl: document.querySelector("button[data-create]"),
+  destroyEl: document.querySelector("button[data-destroy]"),
+  boxesEl: document.querySelector("#boxes"),
+};
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const elem = document.createElement("div");
+
+    elem.style.width = `${30 + 10 * i}px`;
+    elem.style.height = `${30}px`;
+    elem.style.backgroundColor = getRandomHexColor();
+
+    refs.boxesEl.append(elem);
+  }
+}
+
+refs.createEl.addEventListener("click", function () {
+  const amount = refs.amountEl.value;
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+});
+
+refs.destroyEl.addEventListener("click", function () {
+  refs.boxesEl.innerHTML = "";
+});
