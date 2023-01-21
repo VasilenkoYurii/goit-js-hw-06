59cf3e24813c65623e309766ac1d2ae7a6cab1ab
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-01-22 00:05:16 +0200

    ready ninth

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeClr = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const bodyColor = document.querySelector("body");
+
+btnChangeClr.addEventListener("click", changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+
+  spanColor.textContent = `- ${color}`;
+  bodyColor.style.backgroundColor = `${color}`;
+}
