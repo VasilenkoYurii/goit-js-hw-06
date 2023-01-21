0ce957104a148f34f53199e756b6914200e24136
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-01-21 23:38:42 +0200

    ready eighth

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const userObj = {
+    email,
+    password,
+  };
+
+  if (email === "" && password === "") {
+    return alert("Заполни форму");
+  } else if (email === "") {
+    return alert("Заполни email");
+  } else if (password === "") {
+    return alert("Заполни password");
+  }
+
+  form.reset();
+  console.log(userObj);
+}
